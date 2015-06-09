d5db13d0cc01030da5720fe2be7d2c426eafd5e7
Valérian Galliat <val@codejam.info>
2015-06-09 11:07:34 +0200

    Comply to JavaScript Standard Style

--- index.es6.js
+++ index.es6.js
@@ -2,17 +2,20 @@ import flow from 'lodash.flow'
 import hljs from 'highlight.js'
 
 const maybe = f => (...args) => {
-  try { return f(...args) }
-  catch (e) { return false }
+  try {
+    return f(...args)
+  } catch (e) {
+    return false
+  }
 }
 
 const get = name => x => x[name]
 const maybeValue = f => maybe(flow(f, get('value')))
 
 const highlight = (code, lang) =>
-  maybeValue(hljs.highlight)(lang, code, true)
-    || maybeValue(hljs.highlightAuto)(code)
-    || ''
+  maybeValue(hljs.highlight)(lang, code, true) ||
+    maybeValue(hljs.highlightAuto)(code) ||
+    ''
 
 export default md => {
   md.options.highlight = highlight
